5a6f9bc8e1521ede35e8ea4a7f3549760f15e0e4
williamrusdyputra <williamrusdyputra@gmail.com>
2020-06-02 23:15:47 +0700

    modified

--- mountain_carc.py
+++ mountain_carc.py
@@ -4,19 +4,21 @@ from agents.a2c import A2CAgent
 env = gym.make("MountainCarContinuous-v0")
 agent = A2CAgent(env)
 
-agent.start_train()
+agent.train()
 
 # testing the agent
 observation = env.reset()
 terminated = False
 total_reward = 0
+step = 1
 
 while not terminated:
     env.render()
     action = agent.choose_action(observation)
     observation, reward, terminated, _ = env.step(action)
     total_reward += reward
+    step += 1
 
     if terminated:
-        print("TESTING FINISHED WITH REWARD: {}".format(total_reward))
+        print("TESTING FINISHED WITH REWARD: {} and {} step".format(total_reward, step))
         break
