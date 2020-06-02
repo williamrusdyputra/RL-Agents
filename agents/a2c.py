897f2bcc65cca71db0999a3b5dc3201a04883ad8
williamrusdyputra <williamrusdyputra@gmail.com>
2020-06-02 23:12:09 +0700

    A2C solved

--- agents/a2c.py
+++ agents/a2c.py
@@ -1,4 +1,3 @@
-import threading
 import numpy as np
 import tensorflow as tf
 from sklearn import preprocessing
@@ -7,6 +6,13 @@ from .agent_utils import build_actor, build_critic
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+'''
+    On first several episodes your agent should already reached the finish line and
+    get reward above 80 or 90. If your agent is idle on the middle of the mountain and
+    never finish the environment, it means that it got stuck at local optima and you should
+    re-run this program.
+'''
+
 
 class A2CAgent:
     def __init__(self, env):
@@ -14,11 +20,11 @@ class A2CAgent:
         self.env = env
         self.state_shape = env.observation_space.shape
         self.action_space = env.action_space.shape[0]
-        self.n_agent = 4
         self.global_actor = self.build_network('actor')
         self.global_critic = self.build_network('critic')
-        self.barrier = threading.Barrier(self.n_agent)
-        self.max_time = 5e2
+        self.target_critic = self.build_network('critic')
+        self.max_episode = 40
+        self.max_time = 1e3
         self.discount = 0.95
         self.state_space_samples = np.array([env.observation_space.sample() for _ in range(10000)])
         self.scaler = preprocessing.StandardScaler()
@@ -29,6 +35,7 @@ class A2CAgent:
             model = build_actor(self.action_space, self.state_shape)
             try:
                 model.load_weights(self.path[0])
+                print('Model loaded')
             except ValueError:
                 print('Model initialized')
             return model
@@ -36,6 +43,7 @@ class A2CAgent:
             model = build_critic(self.state_shape)
             try:
                 model.load_weights(self.path[1])
+                print('Model loaded')
             except ValueError:
                 print('Model initialized')
             return model
@@ -44,32 +52,20 @@ class A2CAgent:
         observation = self.scale_state(observation.reshape(-1, 2))
         return self.global_actor.predict(observation)
 
-    def create_workers(self):
-        workers = []
-        for i in range(self.n_agent):
-            worker_actor = build_actor(self.action_space, self.state_shape)
-            worker_critic = build_critic(self.state_shape)
-            workers.append([worker_actor, worker_critic])
-        return workers
+    def loss_actor(self, model, inputs, advantage):
+        return -tf.math.log(model(inputs) + 1e-6) * advantage
 
     def loss_critic(self, model, inputs, targets):
         error = targets - model(inputs)
         return tf.reduce_mean(tf.square(error))
 
-    def loss_actor(self, model, inputs, targets):
-        advantage = targets
-        return -tf.math.log(model(inputs) + 1e-6) * advantage
-
     def scale_state(self, state):
         scaled = self.scaler.transform(state)
         return scaled
 
-    def train(self, actor, critic):
-        for i in range(20):
-            print('{} is running on episode {}'.format(threading.current_thread().name, i+1))
-            actor.set_weights(self.global_actor.get_weights())
-            critic.set_weights(self.global_critic.get_weights())
-
+    def train(self):
+        step = 0
+        for i in range(int(self.max_episode)):
             observation = self.env.reset()
             terminated = False
 
@@ -80,10 +76,12 @@ class A2CAgent:
             total_reward = 0
 
             while not terminated and time_step < self.max_time:
+                # self.env.render()
+
                 observation = self.scale_state(observation.reshape(-1, 2))
                 observations.append(observation)
 
-                action = actor.predict(observation)
+                action = self.global_actor.predict(observation)
 
                 observation, reward, terminated, _ = self.env.step(action)
 
@@ -93,32 +91,29 @@ class A2CAgent:
                 total_reward += reward
                 time_step += 1
 
-            print('{} finished with reward: {} and {} step'.format(
-                threading.current_thread().name, total_reward, time_step))
-
             if terminated:
                 return_estimation = 0
             else:
                 observation = self.scale_state(observation.reshape(-1, 2))
-                return_estimation = critic.predict(observation)
+                return_estimation = self.target_critic.predict(observation)
 
             actor_gradients = None
             critic_gradients = None
-            for j in range(len(observations)):
+            for j in range(len(observation)-1, -1, -1):
                 return_estimation = self.discount * return_estimation + rewards[j]
-                td_error = return_estimation - critic.predict(observations[j])
+                td_error = return_estimation - self.target_critic.predict(observations[j])
                 with tf.GradientTape(persistent=True) as tape:
-                    actor_loss = self.loss_actor(actor, observations[j], td_error)
-                    critic_loss = self.loss_critic(critic, observations[j], return_estimation)
+                    actor_loss = self.loss_actor(self.global_actor, observations[j], td_error)
+                    critic_loss = self.loss_critic(self.global_critic, observations[j], return_estimation)
 
                 # accumulate gradients w.r.t parameters
-                actor_gradient = tape.gradient(actor_loss, actor.trainable_weights)
+                actor_gradient = tape.gradient(actor_loss, self.global_actor.trainable_weights)
                 if actor_gradients is None:
                     actor_gradients = actor_gradient
                 else:
                     actor_gradients += actor_gradient
 
-                critic_gradient = tape.gradient(critic_loss, critic.trainable_weights)
+                critic_gradient = tape.gradient(critic_loss, self.global_critic.trainable_weights)
                 if critic_gradients is None:
                     critic_gradients = critic_gradient
                 else:
@@ -129,24 +124,14 @@ class A2CAgent:
             optimizer = self.global_critic.optimizer
             optimizer.apply_gradients(zip(critic_gradients, self.global_critic.trainable_weights))
 
-            print('{} already accumulated gradient. Waiting for other thread..'.format(threading.current_thread().name))
-            self.barrier.wait()
+            step += 1
 
-    def assign_workers(self):
-        workers = self.create_workers()
-        threads = []
-        for idx, worker in enumerate(workers):
-            thread = threading.Thread(target=self.train, args=(worker[0], worker[1]), name='Worker {}'.format(idx+1))
-            threads.append(thread)
+            if step % 10 == 0:
+                self.target_critic.set_weights(self.global_critic.get_weights())
 
-        for thread in threads:
-            thread.start()
+            print('Episode {} finished with reward: {} and {} step'.format(
+                i+1, total_reward, time_step))
 
-        for thread in threads:
-            thread.join()
-
-    def start_train(self):
-        self.assign_workers()
         self.global_actor.save_weights(self.path[0])
         self.global_critic.save_weights(self.path[1])
         print('FINISHED TRAINING')
--- agents/agent_utils.py
+++ agents/agent_utils.py
@@ -4,18 +4,12 @@ import tensorflow as tf
 
 def build_actor(action_space, state_shape):
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(units=128, activation='tanh', input_shape=state_shape),
-        tf.keras.layers.Dense(units=256, activation='tanh'),
+        tf.keras.layers.Dense(units=32, activation='tanh', input_shape=state_shape),
+        tf.keras.layers.Dense(units=64, activation='tanh'),
         tf.keras.layers.Dense(units=action_space, activation='linear')
     ])
 
-    # custom loss function with 1e-6 so log(0) = -inf will not happen
-    def loss(y_true, y_pred):
-        action_true = y_true[:, :action_space]
-        advantage = y_true[:, action_space:]
-        return -np.log(y_pred.prob(action_true) + 1e-6) * advantage
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4), loss=loss)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=5e-4))
 
     return model
 
