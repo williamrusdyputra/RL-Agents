176610ffb5e9f9be285996d16014e0716ab74a94
williamrusdyputra <williamrusdyputra@gmail.com>
2020-06-05 15:22:51 +0700

    modified

--- agents/ppo.py
+++ agents/ppo.py
@@ -15,18 +15,21 @@ class PPOAgent:
         self.discount = 0.97
         self.s_lambda = 0.95
         self.epsilon = 0.2
-        self.rewards = deque(maxlen=128)
-        self.observations = deque(maxlen=256)
+        self.horizon = 256
+        self.batch_size = 32
+        self.epochs = 10
+        self.rewards = deque(maxlen=self.horizon)
+        self.observations = deque(maxlen=self.horizon)
 
     def choose_action(self, observation):
         return self.actor.predict(observation)
 
-    def loss_objective(self, inputs, advantage, reward, observation, new_observation):
-        actor_prob = self.actor.predict(inputs)
-        old_actor_prob = self.old_actor.predict(inputs)
+    def loss_objective(self, advantage, reward, observation, new_observation):
+        actor_prob = self.actor.predict(observation)
+        old_actor_prob = self.old_actor.predict(observation)
         ratio = actor_prob / old_actor_prob
         first_term = ratio * advantage
-        second_term = tf.keras.backend.clip(ratio, 1-self.epsilon, 1+self.epsilon) * advantage
+        second_term = tf.keras.backend.clip(ratio, 1 - self.epsilon, 1 + self.epsilon) * advantage
         actor_loss = tf.math.minimum(first_term, second_term)
         target = reward + self.discount * self.critic.predict(new_observation)
         critic_loss = tf.math.reduce_mean(tf.math.square(target - self.critic.predict(observation)))
@@ -34,17 +37,46 @@ class PPOAgent:
         return critic_loss - actor_loss - entropy
 
     def update_network(self, terminal_step):
-        general_advantage_estimations = []
+        general_advantage_estimations = deque()
+        return_estimations = deque()
         time = 0
 
         for reward in self.rewards[::-1]:
             if time+1 == terminal_step:
+                return_estimation = reward
                 delta = reward - self.critic.predict(self.observations[time])
             else:
-                delta = (reward + self.discount * self.critic.predict(self.observations[time+1]) -
-                         self.critic.predict(self.observations[time]))
+                return_estimation = reward + self.discount * self.critic.predict(self.observations[time+1])
+                delta = return_estimation - self.critic.predict(self.observations[time])
             gae = delta + self.discount * self.s_lambda * general_advantage_estimations[time+1]
             general_advantage_estimations.append(gae)
+            return_estimations.append(return_estimation)
             time += 1
 
+        return_estimations.reverse()
         general_advantage_estimations.reverse()
+
+        # update the actor
+        index = 0
+        step = self.horizon // self.batch_size
+        for i in range(step-1):
+            actor_gradients = None
+            for j in range(self.batch_size):
+                with tf.GradientTape as tape:
+                    loss = self.loss_objective(general_advantage_estimations[index], self.rewards[index],
+                                               self.observations[index], self.observations[index+1])
+
+                actor_gradient = tape.gradient(loss, self.actor.trainable_weights)
+                if actor_gradients is None:
+                    actor_gradients = actor_gradient
+                else:
+                    actor_gradients += actor_gradient
+
+                index += 1
+
+            optimizer = self.actor.optimizer
+            optimizer.apply_gradients(zip(actor_gradients, self.actor.trainable_weights))
+
+            # update the critic
+            self.critic.train_on_batch(self.observations[index-self.batch_size:index],
+                                       return_estimations[index-self.batch_size:index])
