b4142dd0d60c6953e5856ffa575d97d3e22dbc18
williamrusdyputra <williamrusdyputra@gmail.com>
2020-06-07 18:53:26 +0700

    modified

--- agents/ppo.py
+++ agents/ppo.py
@@ -14,26 +14,26 @@ class PPOAgent:
         self.clip = 0.2
         self.epochs = 100
         self.std = 0.4
-        self.state_space = env.observation_space.shape[0]
+        self.observation_space = env.observation_space.shape[0]
         self.action_space = env.action_space.shape[0]
         self.MseLoss = nn.MSELoss()
 
-        self.policy = ActorCritic(self.state_space, self.action_space, self.std).to(device)
+        self.policy = ActorCritic(self.observation_space, self.action_space, self.std).to(device)
         self.load_policy()
         self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=self.learning_rate)
 
-        self.old_policy = ActorCritic(self.state_space, self.action_space, self.std).to(device)
-        self.old_policy.load_state_dict(self.policy.state_dict())
+        self.old_policy = ActorCritic(self.observation_space, self.action_space, self.std).to(device)
+        self.old_policy.load_observation_dict(self.policy.observation_dict())
 
     def load_policy(self):
         try:
-            self.policy.load_state_dict(torch.load(self.path))
+            self.policy.load_observation_dict(torch.load(self.path))
         except FileNotFoundError:
             print('Initialized Model')
 
-    def choose_action(self, state, memory):
-        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
-        return self.old_policy.choose_action(state, memory).cpu().data.numpy().flatten()
+    def choose_action(self, observation, memory):
+        observation = torch.FloatTensor(observation.reshape(1, -1)).to(device)
+        return self.old_policy.choose_action(observation, memory).cpu().data.numpy().flatten()
 
     def update(self, memory):
         rewards = []
@@ -48,21 +48,21 @@ class PPOAgent:
         rewards = torch.tensor(rewards).to(device)
         rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
 
-        old_states = torch.squeeze(torch.stack(memory.observations).to(device), 1).detach()
+        old_observations = torch.squeeze(torch.stack(memory.observations).to(device), 1).detach()
         old_actions = torch.squeeze(torch.stack(memory.actions).to(device), 1).detach()
         old_log_probs = torch.squeeze(torch.stack(memory.log_probs), 1).to(device).detach()
 
         for _ in range(self.epochs):
-            log_probs, state_values, entropy = self.policy.evaluate(old_states, old_actions)
+            log_probs, observation_values, entropy = self.policy.evaluate(old_observations, old_actions)
 
             ratio = torch.exp(log_probs - old_log_probs.detach())
-            advantages = rewards - state_values.detach()
+            advantages = rewards - observation_values.detach()
             surrogate1 = ratio * advantages
             surrogate2 = torch.clamp(ratio, 1 - self.clip, 1 + self.clip) * advantages
-            loss = -torch.min(surrogate1, surrogate2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * entropy
+            loss = -torch.min(surrogate1, surrogate2) + 0.5 * self.MseLoss(observation_values, rewards) - 0.01 * entropy
 
             self.optimizer.zero_grad()
             loss.mean().backward()
             self.optimizer.step()
 
-        self.old_policy.load_state_dict(self.policy.state_dict())
+        self.old_policy.load_observation_dict(self.policy.state_dict())
